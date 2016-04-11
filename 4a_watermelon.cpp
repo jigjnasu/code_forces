7d20cfe36a07d1510689e6a93a7fb4043325d8bc
Rakesh Kumar <T_RakeshK3@verifone.com>
2016-04-11 10:03:41 +0100

    4 A Watermelon solution

---
+++ 4a_watermelon.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main() {
+	int weight = 0;
+	scanf("%d", &weight);
+
+	if (weight > 2) {
+		if (weight % 2)
+			printf("NO\n");
+		else
+			printf("YES\n");
+	} else {
+		printf("NO\n");
+	}
+
+	return 0;
+}
