b542177ff7965831ae5ee5ce366d29a8ac3ae0d5
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-01-14 09:07:06 +1100

    theatre square

---
+++ problem_set/contest/1/theatre_square.cpp
@@ -0,0 +1,27 @@
+/*
+  Contest 1
+  Problem A : Theatre Square
+  Date: 14/01/2020
+ */
+
+#include <iostream>
+
+using ll = unsigned long long int;
+
+int main() {
+    ll m = 0;
+    ll n = 0;
+    ll a = 0;
+    std::cin >> m >> n >> a;
+
+    ll rows = m / a;
+    if (m % a)
+        ++rows;
+
+    ll cols = n / a;
+    if (n % a)
+        ++cols;
+
+    std::cout << rows * cols << std::endl;
+    return 0;
+}
