c720d86fab1830d0e757ef9d5c5f09214316554e
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-06-22 01:06:27 +1000

    duff and meat

---
+++ problem_set/contest/588/duff_and_meat.cpp
@@ -0,0 +1,33 @@
+/*
+  Contest: 588
+  Problem A : Duff and meat
+  Author : Rakesh Kumar
+  Date: 22/06/2020
+*/
+
+#include <bits/stdc++.h>
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+
+    int a = 0;
+    int p = 0;
+    std::cin >> a >> p;
+    --n;
+    long long int cost = a * p;
+    while (n--) {
+        int price = 0;
+        std::cin >> a >> price;
+        if (price < p) {
+            cost += a * price;
+            p = price;
+        } else {
+            cost += a * p;
+        }
+    }
+
+    std::cout << cost << std::endl;
+
+    return 0;
+}
