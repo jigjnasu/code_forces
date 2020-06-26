3ddaeac3ef8912669e4a86fbcd06b5ba21e7b1af
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-06-26 23:39:58 +1000

    game with sticks

--- problem_set/contest/451/game_with_sticks.cpp
+++ problem_set/contest/451/game_with_sticks.cpp
@@ -14,19 +14,10 @@ int main() {
     int m = 0;
     std::cin >> n >> m;
 
-    int i = 0;
-    while (1) {
-        if (n >= 1 && m >= 1) {
-            --n; --m;
-        } else {
-            if ((i & 1) == 0)
-                std::cout << "Malvika" << std::endl;
-            else
-                std::cout << "Akshat" << std::endl;
-            return 0;
-        }
-        ++i;
-    }
+    if ((std::min(n, m) & 1) == 1)
+        std::cout << "Akshat" << std::endl;
+    else
+        std::cout << "Malvika" << std::endl;
 
     return 0;
 }
