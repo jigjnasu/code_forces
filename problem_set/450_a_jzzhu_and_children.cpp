edc0341628c9eebc6fa2727f1dc62462eea2c16a
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-07-06 05:02:02 +1000

    jzzhu and children

---
+++ problem_set/450_a_jzzhu_and_children.cpp
@@ -0,0 +1,31 @@
+/*
+  Contest: 450
+  Problem A : Jzzhu and children
+  Author : Rakesh Kumar
+  Date: 06/07/2020
+*/
+
+#include <bits/stdc++.h>
+
+int main() {
+    int n = 0;
+    double m = 0;
+    std::cin >> n >> m;
+    std::vector<int> kids;
+    int a = 0;
+    std::cin >> a;
+    int max = std::ceil(a / m);
+    int kid = 1;
+    for (int i = 2; i <= n; ++i) {
+        int a = 0;
+        std::cin >> a;
+        if (std::ceil(a / m) >= max) {
+            max = std::ceil(a / m);
+            kid = i;
+        }
+    }
+
+    std::cout << kid << std::endl;
+
+    return 0;
+}
