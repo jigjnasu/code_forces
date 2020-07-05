e2812a079c7e0b16afd50fa62705b7c2f6b0a526
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-07-05 15:17:24 +1000

    lunch rush

---
+++ problem_set/276_a_lunch_rush.cpp
@@ -0,0 +1,30 @@
+/*
+  Contest : 1228
+  Problem A : Disticnt Digits
+  Author : Rakesh Kumar
+  Date: 05/07/2020
+*/
+
+#include <bits/stdc++.h>
+
+using ll = long long int;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    ll n = 0;
+    ll k = 0;
+    std::cin >> n >> k;
+
+    ll f = 0;
+    ll t = 0;
+    std::cin >> f >> t; --n;
+    ll joy = t > k ? f - (t - k) : f;
+    while (n--) {
+        std::cin >> f >> t;
+        joy = std::max(joy, t > k ? f - (t - k) : f);
+    }
+    std::cout << joy << std::endl;
+
+    return 0;
+}
