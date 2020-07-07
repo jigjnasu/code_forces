a8416c96382faa9c3d373d8e22ccf60bc914998b
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-07-08 01:40:57 +1000

    remove a progression

---
+++ problem_set/1194_a_remove_a_progression.cpp
@@ -0,0 +1,27 @@
+/*
+  Contest: 1194
+  Problem A : Remove a progression
+  Author : Rakesh Kumar
+  Date: 08/07/2020
+*/
+
+#include <bits/stdc++.h>
+
+using ll = long long int;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int t = 0;
+    std::cin >> t;
+
+    while (t--) {
+        ll n = 0;
+        ll x = 0;
+        std::cin >> n >> x;
+        std::cout << (x << 1) << std::endl;
+    }
+
+    return 0;
+}
