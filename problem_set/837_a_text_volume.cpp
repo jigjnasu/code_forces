28c1ac00f43afedeeb3608fc41a008afac920510
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-07-29 23:50:59 +1000

    Text volume

---
+++ problem_set/837_a_text_volume.cpp
@@ -0,0 +1,15 @@
+/*
+  Contest : 837
+  Problem A : Text Volume
+  Author : Rakesh Kumar
+  Date: 29/07/2020
+*/
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    return 0;
+}
