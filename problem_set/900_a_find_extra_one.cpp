3459245af0cacaa199de84b70372e0e631d3bc91
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-07-30 04:33:04 +1000

    find extra one

--- problem_set/900_a_find_extra_one.cpp
+++ problem_set/900_a_find_extra_one.cpp
@@ -8,6 +8,25 @@
 #include <bits/stdc++.h>
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0;
+    std::cin >> n;
+    int positive = 0;
+    int negative = 0;
+    for (int i = 0; i < n; ++i) {
+        int x = 0, y = 0;
+        std::cin >> x >> y;
+        if (x > 0)
+            ++positive;
+        else
+            ++negative;
+    }
+    std::string res = "No";
+    if (positive >= n - 1 || negative >= n - 1)
+        res = "Yes";
+    std::cout << res << std::endl;
 
     return 0;
 }
