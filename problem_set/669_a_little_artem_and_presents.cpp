f15e92d2c13898f37b632a5f87548bfd376a9ebd
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-07-30 05:03:28 +1000

    little artem and presents

--- problem_set/669_a_little_artem_and_presents.cpp
+++ problem_set/669_a_little_artem_and_presents.cpp
@@ -8,6 +8,15 @@
 #include <bits/stdc++.h>
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0;
+    std::cin >> n;
+    int r = (n / 3) << 1;
+    if (n % 3)
+        ++r;
+    std::cout << r << std::endl;
 
     return 0;
 }
