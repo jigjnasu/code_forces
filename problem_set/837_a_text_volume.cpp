583cc903c211a64032da5ff59214f8a005ac57fa
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-07-30 05:09:28 +1000

    text volume

--- problem_set/837_a_text_volume.cpp
+++ problem_set/837_a_text_volume.cpp
@@ -11,5 +11,24 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0); std::cout.tie(0);
 
+    int n = 0;
+    std::cin >> n;
+    std::cin.ignore();
+    std::string s;
+    std::getline(std::cin, s);
+    int max = 0;
+    int count = 0;
+    for (char c : s) {
+        if (c >= 'A' && c <= 'Z')
+            ++count;
+        if (c == ' ') {
+            max = std::max(max, count);
+            count = 0;
+        }
+    }
+
+    max = std::max(max, count);
+    std::cout << max << std::endl;
+
     return 0;
 }
