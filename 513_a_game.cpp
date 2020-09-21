f5609670793c6639a27bafe7ac64cecc9554d8f0
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-09-21 14:06:16 +1000

    game

---
+++ 513_a_game.cpp
@@ -0,0 +1,22 @@
+/*
+  Contest : 513
+  Problem A : Game
+  Author : Rakesh Kumar
+  Date: 21/09/2020
+*/
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n1 = 0, k1 = 0, n2 = 0, k2 = 0;
+    std::cin >> n1 >> n2 >> k1 >> k2;
+    if (n1 > n2)
+        std::cout << "First" << std::endl;
+    else
+        std::cout << "Second" << std::endl;
+
+    return 0;
+}
