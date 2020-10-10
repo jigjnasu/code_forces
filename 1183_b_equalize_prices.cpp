4bcd99cd2f7b8ee92fe83c619a0a83c0e9060f61
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-10 14:13:24 +1100

    equalize prices

---
+++ 1183_b_equalize_prices.cpp
@@ -0,0 +1,42 @@
+/*
+  Contest : 1003
+  Problem A : Polycrap's pockets
+  Author : Rakesh Kumar
+  Date: 14/06/2020
+*/
+
+#include <bits/stdc++.h>
+
+using ll = long long int;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int q = 0;
+    std::cin >> q;
+    while (q--) {
+        int n = 0, k = 0;
+        std::cin >> n >> k;
+        ll min = 0;
+        std::cin >> min; --n;
+        std::vector<ll> v;
+        v.emplace_back(min);
+        while (n--) {
+            ll e = 0;
+            std::cin >> e;
+            min = std::min(e, min);
+            v.emplace_back(e);
+        }
+        ll r = min + k;
+        for (int e : v) {
+            if (std::abs(r - e) > k) {
+                r = -1;
+                break;
+            }
+        }
+        std::cout << r << std::endl;
+    }
+
+    return 0;
+}
