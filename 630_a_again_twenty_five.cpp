e2f9b7438e2306592e11dc0b8e2774f51e08f660
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-04-11 23:17:17 +1000

    again twenty five!

---
+++ 630_a_again_twenty_five.cpp
@@ -0,0 +1,19 @@
+/*
+  Contest : 630
+  Problem A : Again Twenty Five!
+  Author : Rakesh Kumar
+  Date: 11/04/2021
+*/
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0;
+    std::cin >> n;
+    std::cout << 25 << std::endl;
+
+    return 0;
+}
